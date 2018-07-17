a4b64657142d1797f0e33b67d820e97f39b10cdc
Nate Wooten <njwooten@outlook.com>
2018-07-16 21:16:05 -0400

    Modify README.md

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -35,6 +35,7 @@ $(function() {
           allFeeds.forEach(function(feed) {
           expect(feed.url).toBeDefined();
           expect(feed.url.length).not.toBe(0);
+          expect(feed.url).toMatch(/^(http|https):\/\//);
         });
       });
 
